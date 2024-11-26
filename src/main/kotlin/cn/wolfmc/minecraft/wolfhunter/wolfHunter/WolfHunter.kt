352a2ac2acc5c4325ec915992189b514dd29af2c
MikkoAyaka <11067779+mikkoayaka@user.noreply.gitee.com>
2024-11-26 16:21:17 +0800

    :tada: 初始化项目仓库

---
+++ src/main/kotlin/cn/wolfmc/minecraft/wolfhunter/wolfHunter/WolfHunter.kt
@@ -0,0 +1,14 @@
+package cn.wolfmc.minecraft.wolfhunter.wolfHunter
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WolfHunter : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
