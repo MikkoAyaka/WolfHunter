5b65208bb9f374ee8ef565e1a57949907071da01
MikkoAyaka <11067779+mikkoayaka@user.noreply.gitee.com>
2024-12-01 11:36:47 +0800

    :recycle: 使用 TabooLib Command 替换原有的 CommandDSL

--- src/main/kotlin/cn/wolfmc/minecraft/wolfhunter/presentation/command/WolfHunterCommand.kt
+++ src/main/kotlin/cn/wolfmc/minecraft/wolfhunter/presentation/command/WolfHunterCommand.kt
@@ -1,52 +1,46 @@
 package cn.wolfmc.minecraft.wolfhunter.presentation.command
 
-import cn.wolfmc.minecraft.wolfhunter.common.extensions.command
 import cn.wolfmc.minecraft.wolfhunter.common.extensions.openMenu
-import cn.wolfmc.minecraft.wolfhunter.infrastructure.mechanism.ItemSpawnHandler
 import cn.wolfmc.minecraft.wolfhunter.presentation.item.ScaffoldBlock
 import cn.wolfmc.minecraft.wolfhunter.presentation.menu.mainMenu
 import cn.wolfmc.minecraft.wolfhunter.presentation.menu.testMenu
 import org.bukkit.entity.Player
-import org.bukkit.event.entity.CreatureSpawnEvent
-import org.bukkit.plugin.java.JavaPlugin
+import taboolib.common.platform.command.CommandBody
+import taboolib.common.platform.command.CommandHeader
+import taboolib.common.platform.command.subCommand
 
-fun registerCommands(plugin: JavaPlugin) {
-    plugin.command("wolfhunter") {
-        argument("menu") { arg ->
-            when (arg) {
-                "main" -> openMenu(mainMenu)
-                "test" -> openMenu(testMenu)
-            }
-        }
-        literal("debug") {
-            literal("toggle") {
-                literal("invulnerable") {
-                    runs {
-                        if (this !is Player) return@runs
-                        this.isInvulnerable = !this.isInvulnerable
+@CommandHeader("wolfhunter", ["wh", "haw", "hw"])
+object WolfHunterCommand {
+    @CommandBody
+    val menu =
+        subCommand {
+            dynamic {
+                execute<Player> { sender, _, arg ->
+                    when (arg) {
+                        "main" -> sender.openMenu(mainMenu)
+                        "test" -> sender.openMenu(testMenu)
                     }
                 }
             }
+        }
+
+    @CommandBody
+    val debug =
+        subCommand {
+            literal("toggle-invulnerable") {
+                execute<Player> { sender, _, _ ->
+                    sender.isInvulnerable = !sender.isInvulnerable
+                }
+            }
             literal("inv") {
-                runs {
-                    if (this !is Player) return@runs
-                    this.inventory.forEachIndexed { index, itemStack -> println("$index: $itemStack") }
+                execute<Player> { sender, _, _ ->
+                    sender.inventory.forEachIndexed { index, itemStack -> println("$index: $itemStack") }
                 }
             }
-            literal("scaffold") {
-                runs {
-                    if (this !is Player) return@runs
-                    ScaffoldBlock.giveItem(this)
-                }
-            }
-            literal("listener") {
-                runs {
-                    CreatureSpawnEvent.getHandlerList().registeredListeners.forEach { println(it.javaClass.simpleName) }
-                    ItemSpawnHandler.disable()
-                    println("---")
-                    CreatureSpawnEvent.getHandlerList().registeredListeners.forEach { println(it.javaClass.simpleName) }
+            literal("scaffold-block") {
+                execute<Player> { sender, _, _ ->
+                    ScaffoldBlock.giveItem(sender)
                 }
             }
         }
-    }
 }
